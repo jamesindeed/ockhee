d9d4e2566dd651008f6e0af31ac34dd4b5422db3
James Troughton <jamesstroughton@gmail.com>
2021-11-11 11:37:39 +1300

    👽️ Fetch POSTS

---
+++ services/index.js
@@ -0,0 +1,40 @@
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const POSTS = gql`
+    query getPosts {
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await request(graphqlAPI, POSTS);
+
+  return data.postsConnection.edges;
+};
