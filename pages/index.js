da04075a00f082fc521b9548c893fc0e5cab71f5
James Troughton <jamesstroughton@gmail.com>
2021-11-17 15:52:42 +1300

    💄 Home UI

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,19 @@ import { getPosts } from "../services";
 
 export default function Home({ posts }) {
   return (
-    <div className="max-w-7xl container mx-auto px-10 mb-8">
+    <div className="container px-10 mx-auto mb-8 max-w-7xl">
       <Head>
         <title>Ockhee</title>
         <link rel="icon" href="/letter-o.png" />
       </Head>
-      <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-12 gap-8 mt-8">
-        <div className="lg:col-span-8 md:col-span-5 col-span-1">
+      <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-8 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8 md:col-span-5">
           {posts.map((post, index) => (
             <CardAlt post={post.node} key={index} />
           ))}
         </div>
-        <div className="lg:col-span-4 md:col-span-3 col-span-1">
-          <div className="lg:sticky md:sticky relative md:top-8">
+        <div className="col-span-1 lg:col-span-4 md:col-span-3">
+          <div className="relative lg:sticky md:sticky md:top-8">
             <PostWidget />
             <Categories />
           </div>
