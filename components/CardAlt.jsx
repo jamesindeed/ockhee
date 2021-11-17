79c27324a84b15953622a9756379ef1e4181a9e7
James Troughton <jamesstroughton@gmail.com>
2021-11-17 15:50:20 +1300

    💄 Card UI

--- components/CardAlt.jsx
+++ components/CardAlt.jsx
@@ -5,32 +5,32 @@ import moment from "moment";
 const CardAlt = ({ post }) => {
   return (
     <Link href={`/post/${post.slug}`}>
-      <div className="card overflow-hidden cursor-pointer hover:-translate-y-1 transition-all duration-00 bg-white border border-gray-200 rounded-lg p-0 lg:p-6 pb-6 mb-8">
-        <div className="relative overflow-hidden pb-60 mb-6 rounded-t-lg lg:rounded-lg">
+      <div className="p-0 pb-6 mb-8 overflow-hidden transition-all bg-white border border-gray-200 rounded-lg cursor-pointer card hover:-translate-y-1 duration-00 lg:p-6">
+        <div className="relative mb-6 overflow-hidden rounded-t-lg pb-60 lg:rounded-lg">
           <img
             src={post.featuredImage.url}
             alt={post.title}
-            className="object-top absolute h-60 w-full object-cover shadow-sm"
+            className="absolute object-cover object-top w-full shadow-sm h-60"
           />
         </div>
 
         <div className="px-6 py-2 lg:px-0">
-          <h2 className="text-xl font-bold transition duration-400 hover:text-indigo-500">
+          <h2 className="text-2xl font-bold transition duration-400 hover:text-indigo-500">
             <Link href={`/post/${post.slug}`}>{post.title}</Link>
           </h2>
-          <p className="mt-2 text-sm text-gray-600">{post.excerpt}</p>
-          <div className="flex flex-row mt-4 items-center">
-            <div className="align-middle rounded-full h-8 w-8 overflow-hidden mr-3">
+          <p className="mt-2 text-sm prose text-gray-600">{post.excerpt}</p>
+          <div className="flex flex-row items-center mt-4">
+            <div className="w-8 h-8 mr-3 overflow-hidden align-middle rounded-full">
               <img alt={post.author.name} src={post.author.photo.url} />
             </div>
-            <div className="font-bold mr-2 text-sm">{post.author.name}</div>
-            <div className="text-gray-400 text-xs">
+            <div className="mr-2 text-sm font-bold">{post.author.name}</div>
+            <div className="text-xs text-gray-400">
               {moment(post.createdAt).format("MMM DD, YYYY")}
             </div>
           </div>
           <div className="flex flex-row flex-wrap">
             {post.categories.slice(0, 4).map((category) => (
-              <div className="uppercase text-xs text-indigo-700 bg-indigo-200 px-2 py-1 mt-6 rounded-full mr-2">
+              <div className="px-2 py-1 mt-6 mr-2 text-xs text-indigo-700 uppercase bg-indigo-200 rounded-full">
                 <p>#{category.name}</p>
               </div>
             ))}
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,31 +5,31 @@ import Link from "next/link";
 const PostCard = ({ post }) => {
   return (
     <Link href={`/post/${post.slug}`}>
-      <div className="card cursor-pointer hover:-translate-y-1 transition-all duration-400 bg-white border border-gray-200 rounded-lg p-0 lg:p-6 pb-12 mb-8">
-        <div className="relative overflow-hidden pb-80 mb-6">
+      <div className="p-0 pb-12 mb-8 transition-all bg-white border border-gray-200 rounded-lg cursor-pointer card hover:-translate-y-1 duration-400 lg:p-6">
+        <div className="relative mb-6 overflow-hidden pb-80">
           <img
             src={post.featuredImage.url}
             alt={post.title}
-            className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+            className="absolute object-cover object-top w-full rounded-t-lg shadow-lg h-80 lg:rounded-lg"
           />
         </div>
 
-        <h1 className="px-2 transition duration-400 text-center mb-8 cursor-pointer hover:text-indigo-500 text-2xl font-semibold">
+        <h1 className="px-2 mb-8 text-2xl font-semibold text-center transition cursor-pointer duration-400 hover:text-indigo-500">
           <Link href={`/post/${post.slug}`}>{post.title}</Link>
         </h1>
-        <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
-          <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-            <div className="align-middle rounded-full h-8 w-8 overflow-hidden">
+        <div className="items-center justify-center block w-full mb-8 text-center lg:flex">
+          <div className="flex items-center justify-center w-full mb-4 mr-8 lg:mb-0 lg:w-auto">
+            <div className="w-8 h-8 overflow-hidden align-middle rounded-full">
               <img alt={post.author.name} src={post.author.photo.url} />
             </div>
-            <p className="inline align-middle text-gray-700 ml-2 font-medium text-base">
+            <p className="inline ml-2 text-base font-medium text-gray-700 align-middle">
               {post.author.name}
             </p>
           </div>
-          <div className="font-medium text-base text-gray-700">
+          <div className="text-base font-medium text-gray-700">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 inline mr-2 text-indigo-500"
+              className="inline w-6 h-6 mr-2 text-indigo-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -46,7 +46,7 @@ const PostCard = ({ post }) => {
             </span>
           </div>
         </div>
-        <p className="text-center text-base text-gray-700 font-normal px-5 lg:px-20">
+        <p className="px-5 text-base font-normal text-center text-gray-700 lg:px-20">
           {post.excerpt}{" "}
         </p>
       </div>
