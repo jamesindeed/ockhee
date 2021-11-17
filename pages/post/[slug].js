1e6d390c4483b649ecd509df21ede433f54e4685
James Troughton <jamesstroughton@gmail.com>
2021-11-17 15:51:59 +1300

    ✨ Posts

---
+++ pages/post/[slug].js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import { getPosts, getPostDetails } from "../../services";
+
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+} from "../../components";
+
+const PostDetails = ({ post }) => {
+  return (
+    <div className="container px-10 mx-auto mt-5 mb-8 max-w-7xl">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-8 lg:grid-cols-12 ">
+        <div className="col-span-1 lg:col-span-8 md:col-span-5">
+          <PostDetail post={post} />
+          <Author author={post.author} />
+          <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} />
+        </div>
+        <div className="col-span-1 lg:col-span-4 md:col-span-3">
+          <div className="relative lg:sticky md:sticky md:top-8">
+            <PostWidget
+              slug={post.slug}
+              categories={post.categories.map((category) => category.slug)}
+            />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDetails;
+
+export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug);
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getPosts();
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: false,
+  };
+}
