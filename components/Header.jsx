a48861989e992a84e57e44c23d2b1501053294dd
James Troughton <jamesstroughton@gmail.com>
2021-11-17 15:50:57 +1300

    💄 Header UI

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useEffect, useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import {
-  MenuIcon,
+  MenuAlt3Icon,
   PlayIcon,
   XIcon,
   ChatAlt2Icon,
@@ -45,37 +45,35 @@ export default function Header() {
     getCategories().then((res) => setCategories(res));
   }, []);
 
-  console.log(recentPosts);
-
   return (
     <Popover className="relative bg-white">
       <div className="max-w-full mx-auto">
-        <div className="px-8 flex justify-between items-center border-b border-gray-200 py-3 md:space-x-10">
+        <div className="flex items-center justify-between px-8 py-3 border-b border-gray-200 md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="/">
               <span className="sr-only">Logo</span>
               <img
-                className="h-10 w-auto"
+                className="w-auto h-10"
                 // *Logo
-                src="/letter-o.png"
+                src="/logo1.gif"
                 alt="Logo"
               />
             </a>
           </div>
-          <div className="-mr-2 -my-2">
+          <div className="-my-2 -mr-2">
             {" "}
             {/* md:hidden */}
-            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-black hover:text-black hover:bg-gray-100 focus:outline-none">
+            <Popover.Button className="inline-flex items-center justify-center p-1 m-1 text-black bg-white rounded-md hover:text-black hover:bg-gray-100 focus:outline-none">
               <span className="sr-only">Open menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              <MenuAlt3Icon className="w-7 h-7" aria-hidden="true" />
             </Popover.Button>
           </div>
 
           {/* Contact Button */}
-          {/* <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          {/* <div className="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
             <a
               href="/contact"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-black hover:bg-gray-800"
+              className="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-gray-800"
             >
               Contact
             </a>
@@ -95,36 +93,36 @@ export default function Header() {
       >
         <Popover.Panel
           focus
-          className="z-50 absolute max-w-7xl mx-auto md:pt-5 top-0 inset-x-0 p-2 transition transform origin-top-right"
+          className="absolute inset-x-0 top-0 z-50 p-2 mx-auto transition origin-top-right transform max-w-7xl md:pt-5"
         >
-          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-            <div className="pt-5 pb-6 px-5">
+          <div className="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50">
+            <div className="px-5 pt-5 pb-6">
               <div className="flex items-center justify-between">
                 <div>
                   <img
-                    className="h-10 w-auto"
+                    className="w-auto h-10"
                     // *Logo for mobile menu
-                    src="/letter-o.png"
+                    src="/logo1.gif"
                     alt="Logo"
                   />
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-black hover:text-black hover:bg-gray-100 focus:outline-none">
+                  <Popover.Button className="inline-flex items-center justify-center p-2 text-black bg-white rounded-md hover:text-black hover:bg-gray-100 focus:outline-none">
                     <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                    <XIcon className="w-6 h-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
               <div className="mt-8">
-                <nav className="grid gap-y-8 bg-white">
+                <nav className="grid bg-white gap-y-8">
                   {solutions.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                      className="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50"
                     >
                       <item.icon
-                        className="flex-shrink-0 h-6 w-6 text-black"
+                        className="flex-shrink-0 w-6 h-6 text-black"
                         aria-hidden="true"
                       />
                       <span className="ml-3 text-base font-medium text-gray-900">
@@ -137,54 +135,59 @@ export default function Header() {
                     Catagories
                   </span>
 
-                  {categories.map((category, index) => (
+                  {categories.slice(0, 4).map((category, index) => (
                     <a
                       key={index}
                       href={`/category/${category.slug}`}
-                      className="-m-3 p-3 flex categorys-center rounded-md hover:bg-gray-50"
+                      className="flex p-3 -m-3 rounded-md categorys-center hover:bg-gray-50"
                     >
                       <PlayIcon
-                        className="flex-shrink-0 h-6 w-6 text-black"
+                        className="flex-shrink-0 w-6 h-6 text-black"
                         aria-hidden="true"
                       />
-                      {/* <img className="h-6 w-auto" src={item.icon} alt="Logo" /> */}
-                      <span className="ml-3 capitalize text-base font-medium text-gray-900">
+                      <span className="ml-3 text-base font-medium text-gray-900 capitalize">
                         {category.name}
                       </span>
                     </a>
                   ))}
+
+                  <div className="text-sm">
+                    <a
+                      href="/category"
+                      className="font-medium text-indigo-500 hover:text-indigo-400"
+                    >
+                      {" "}
+                      View all catagories <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </div>
                 </nav>
               </div>
             </div>
 
-            <div className="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
+            <div className="px-5 py-5 rounded-b-lg bg-gray-50">
               <div>
-                <h3 className="text-sm tracking-wide font-medium text-gray-500 uppercase">
+                <h3 className="text-sm font-medium tracking-wide text-gray-500 uppercase">
                   Recent Posts
                 </h3>
                 <ul role="list" className="mt-4 space-y-4">
                   {recentPosts.map((post, index) => (
-                    <li>
-                      <Link
-                        key={index}
-                        href={`/post/${post.slug}`}
-                        className="text-base truncate font-medium text-gray-900 hover:text-gray-700"
-                      >
+                    <li className="text-base font-medium text-gray-900 truncate hover:text-gray-700">
+                      <Link key={index} href={`/post/${post.slug}`}>
                         {post.title}
                       </Link>
                     </li>
                   ))}
                 </ul>
               </div>
-              {/* <div className="mt-5 text-sm">
+              <div className="mt-5 text-sm">
                 <a
-                  href="#"
+                  href="/"
                   className="font-medium text-indigo-500 hover:text-indigo-400"
                 >
                   {" "}
                   View all posts <span aria-hidden="true">&rarr;</span>
                 </a>
-              </div> */}
+              </div>
             </div>
           </div>
         </Popover.Panel>
