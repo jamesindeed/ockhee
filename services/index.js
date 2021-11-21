92b307ab931e8b3e3769eb0e7b34f4f5d5a7aaed
James Troughton <jamesstroughton@gmail.com>
2021-11-21 19:25:26 +1300

    👽️ API Requests

--- services/index.js
+++ services/index.js
@@ -133,3 +133,56 @@ export const getCategories = async () => {
 
   return res.categories;
 };
+
+export const submitComment = async (obj) => {
+  const result = await fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
+
+export const getComments = async (slug) => {
+  const GET_COMMENTS = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, GET_COMMENTS, { slug });
+
+  return result.comments;
+};
+
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetCategoryPost() {
+      posts(where: {featuredPost: true}) {
+        author {
+          name
+          photo {
+            url
+          }
+        }
+        featuredImage {
+          url
+        }
+        title
+        slug
+        createdAt
+      }
+    }   
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
