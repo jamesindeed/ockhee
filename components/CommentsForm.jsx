d80b7bbf1947488175d2585d6a6edf184340affb
James Troughton <jamesstroughton@gmail.com>
2021-11-22 15:01:10 +1300

    🎨 Restructured Code

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,33 +1,52 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { submitComment } from "../services";
 
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const commentEl = useRef();
-  const nameEl = useRef();
-  const emailEl = useRef();
-  const storeDataEl = useRef();
-
-  useEffect(() => {
-    nameEl.current.value = window.localStorage.getItem("name");
-    emailEl.current.value = window.localStorage.getItem("email");
+  const [formData, setFormData] = useState({
+    name: null,
+    email: null,
+    comment: null,
+    storeData: null,
   });
 
-  const handleSubmit = () => {
+  useEffect(() => {
+    setLocalStorage(window.localStorage);
+    const initalFormData = {
+      name: window.localStorage.getItem("name"),
+      email: window.localStorage.getItem("email"),
+      comment: null,
+      storeData:
+        window.localStorage.getItem("name") ||
+        window.localStorage.getItem("email"),
+    };
+    setFormData(initalFormData);
+  }, []);
+
+  const onInputChange = (e) => {
+    const { target } = e;
+    if (target.type === "checkbox") {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }));
+    }
+  };
+
+  const handlePostSubmission = () => {
     setError(false);
-
-    const { value: comment } = commentEl.current;
-    const { value: name } = nameEl.current;
-    const { value: email } = emailEl.current;
-    const { checked: storeData } = storeDataEl.current;
-
+    const { name, email, comment, storeData } = formData;
     if (!name || !email || !comment) {
       setError(true);
       return;
     }
-
     const commentObj = {
       name,
       email,
@@ -36,86 +55,94 @@ const CommentsForm = ({ slug }) => {
     };
 
     if (storeData) {
-      window.localStorage.setItem("name", name);
-      window.localStorage.setItem("email", email);
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
     } else {
-      window.localStorage.removeItem("name");
-      window.localStorage.removeItem("email");
+      localStorage.removeItem("name");
+      localStorage.removeItem("email");
     }
 
     submitComment(commentObj).then((res) => {
-      setShowSuccessMessage(true);
-      setTimeout(() => {
-        setShowSuccessMessage(false);
-      }, 3000);
+      if (res.createComment) {
+        if (!storeData) {
+          formData.name = "";
+          formData.email = "";
+        }
+        formData.comment = "";
+        setFormData((prevState) => ({
+          ...prevState,
+          ...formData,
+        }));
+        setShowSuccessMessage(true);
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 3000);
+      }
     });
   };
 
   return (
     <div className="p-8 pb-12 mb-8 bg-white border border-gray-200 rounded-lg">
-      <h3 className="pb-4 mb-2 text-lg font-semibold">Comments</h3>
+      <h3 className="pb-4 mb-4 text-xl font-semibold">Leave a Comment</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
-          required
-          ref={commentEl}
-          className="w-full p-4 text-sm text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200"
-          placeholder="Comment"
+          value={formData.comment}
+          onChange={onInputChange}
+          className="w-full h-40 p-4 text-sm text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200"
           name="comment"
+          placeholder="Comment"
         />
       </div>
       <div className="grid grid-cols-1 gap-4 mb-4 lg:grid-cols-2">
         <input
-          required
           type="text"
-          ref={nameEl}
-          className="w-full p-4 text-sm text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200"
+          value={formData.name}
+          onChange={onInputChange}
+          className="w-full px-4 py-2 text-sm text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Name"
           name="name"
         />
         <input
-          required
-          type="text"
-          ref={emailEl}
-          className="w-full p-4 text-sm text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200"
+          type="email"
+          value={formData.email}
+          onChange={onInputChange}
+          className="w-full px-4 py-2 text-sm text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Email"
           name="email"
         />
       </div>
-      <div className="grid grid-cols-1 gap-4 mb-4">
+      <div className="grid grid-cols-1 gap-4 mb-4 text-sm">
         <div>
           <input
-            className="ml-1"
-            ref={storeDataEl}
+            checked={formData.storeData}
+            onChange={onInputChange}
             type="checkbox"
-            name="storeData"
             id="storeData"
+            name="storeData"
             value="true"
           />
-          <label
-            className="pl-2 text-sm text-gray-500 cursor-pointer"
-            htmlFor="storeData"
-          >
+          <label className="text-gray-500 cursor-pointer" htmlFor="storeData">
             {" "}
-            Save my name and email for the next time I comment.
+            Save my name, email in this browser for the next time I comment.
           </label>
         </div>
-        {error && (
-          <p className="text-xs text-red-500">All fields are required</p>
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are mandatory</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handlePostSubmission}
+          className="inline-block px-4 py-2 font-medium text-white transition duration-500 bg-black rounded-lg cursor-pointer text-md ease hover:bg-indigo-900"
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-sm font-semibold text-green-500">
+            Comment submitted for review
+          </span>
         )}
-        <div className="mt-2">
-          <button
-            type="button"
-            className="inline-block px-3 py-1 text-white transition duration-500 bg-black rounded-lg cursor-pointer ease hover:bg-indigo-900 text-md"
-            onClick={handleSubmit}
-          >
-            Submit
-          </button>
-          {showSuccessMessage && (
-            <span className="float-right text-sm font-semibold text-green-500">
-              Comment submitted for review.
-            </span>
-          )}
-        </div>
       </div>
     </div>
   );
