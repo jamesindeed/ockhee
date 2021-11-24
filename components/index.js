8b222e5b910964479af3011471909e637bf51b94
James Troughton <jamesstroughton@gmail.com>
2021-11-24 14:24:21 +1300

    ➕ Imports

--- components/index.js
+++ components/index.js
@@ -1,4 +1,3 @@
-export { default as PostCard } from "./PostCard";
 export { default as PostWidget } from "./PostWidget";
 export { default as Categories } from "./Categories";
 export { default as Header } from "./Header";
@@ -10,3 +9,4 @@ export { default as CommentsForm } from "./CommentsForm";
 export { default as Comments } from "./Comments";
 export { default as FeaturedPostCard } from "./FeaturedPostCard";
 export { default as Loader } from "./Loader";
+export { default as NextPostCard } from "./NextPostCard";
