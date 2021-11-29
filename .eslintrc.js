80a2cb6b4a83b3f525dae889d1a87754aa480097
James Troughton <jamesstroughton@gmail.com>
2021-11-29 18:40:58 +1300

    ⚡️ Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'prettier'],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
