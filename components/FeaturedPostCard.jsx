0dd2dadee87b9bb210b5ebf47c441ea1e87c3986
James Troughton <jamesstroughton@gmail.com>
2021-12-07 16:48:37 +1300

    🐛 Fixed Image Stretch

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import moment from 'moment';
-import Image from 'next/image';
 import Link from 'next/link';
 
 const FeaturedPostCard = ({ post }) => (
   <div className='relative overflow-hidden h-72'>
     <div
-      className='absolute inline-block w-full bg-center bg-no-repeat bg-cover border border-gray-100 rounded-lg h-72'
+      className='absolute inline-block object-contain w-full bg-center bg-no-repeat bg-cover border border-gray-100 rounded-lg h-72'
       style={{ backgroundImage: `url('${post.featuredImage.url}')` }}
     />
     <div className='absolute w-full bg-center rounded-lg opacity-50 bg-gradient-to-b from-gray-400 via-gray-700 to-black h-72' />
@@ -17,15 +16,10 @@ const FeaturedPostCard = ({ post }) => (
       <p className='mb-4 text-xl font-semibold text-center text-white text-shadow'>
         {post.title}
       </p>
-      <div className='absolute flex items-center justify-center w-full overflow-hidden bottom-5'>
-        <Image
-          unoptimized
-          alt={post.author.name}
-          height='30px'
-          width='30px'
-          className='align-middle rounded-full drop-shadow-lg'
-          src={post.author.photo.url}
-        />
+      <div className='absolute flex items-center justify-center w-full bottom-5'>
+        <div className='w-8 h-8 overflow-hidden align-middle rounded-full'>
+          <img alt={post.author.name} src={post.author.photo.url} />
+        </div>
         <p className='inline ml-2 text-sm font-medium text-white align-middle text-shadow'>
           {post.author.name}
         </p>
